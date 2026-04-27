2c44e95bd0c3b7ea4a3f6b221f59580a5806d8f6
AMAN SINGH <Amnpanwar17@gmail.com>
2026-04-27 14:01:51 +0530

    Hardcode API URL

--- job-portal/frontend/src/services/api.js
+++ job-portal/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+const API_BASE = 'https://job-portal-4-ll7r.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
